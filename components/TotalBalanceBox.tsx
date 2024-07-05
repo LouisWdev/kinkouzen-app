224fc0250a3468a93b0569e7066f375ae7a835fe
LouisWackenier <Louis.wackenier@gmail.com>
2024-07-05 10:19:10 +0200

    sidebar and active home button

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,4 +1,6 @@
- import AnimatedCounter from './AnimatedCounter'
+ import { Doughnut } from 'react-chartjs-2'
+import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -6,7 +8,7 @@ const TotalBalanceBox = ({
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
-           {/* DoughnutChart */}
+           <DoughnutChart accounts={accounts} />
         </div>
 
         <div className='flex flex-col gap-6'>
@@ -18,9 +20,9 @@ const TotalBalanceBox = ({
                 Total Current Balance
               </p>
 
-              <p className='total-balance-amount flex-center gap-2'>
+              <div className='total-balance-amount flex-center gap-2'>
                  <AnimatedCounter amount={totalCurrentBalance}/>
-              </p>
+              </div>
            </div>
         </div>
     </section>
